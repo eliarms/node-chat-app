97b04773db223d8bb38959ff1a6a1eff149b78fe
Emmanuel Eliason-Armstrong <emmanuel.eliason-armstrong@amadeus.com>
2018-10-06 18:33:02 +0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+//const _ = require('lodash');
+const express = require('express');
+const path = require('path');
+const publicPath = path.join(__dirname,'../public');
+
+var app = express();
+    app.use(express.static(publicPath));
+    app.listen(3000, () =>{
+        console.log('server is up on port 3000');
+
+    } );
